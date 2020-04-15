59f2f28ed06f0aea3f6d665df722a2de6f5e913c
Shadow <1240717848@qq.com>
2020-04-15 13:34:38 -0500

    Implemented more on part 1 and 2..

--- part#1.py
+++ part#1.py
@@ -6,7 +6,7 @@ url='https://raw.githubusercontent.com/efosler/cse5522data/master/vowelfmts.csv'
 df=pd.read_csv(url)
 data = np.array(df)
 
-model = component.two_layer_sigmoid(input_dim=10, hidd_dim=100, output_dim=9)
+model = component.two_layer_sigmoid(input_dim=10, hid_dim=100, output_dim=9)
 
 
 
--- part#2.py
+++ part#2.py
@@ -7,8 +7,11 @@ df=pd.read_csv(url)
 data = np.array(df)
 
 
+model_1 = component.two_layer_sigmoid(input_dim=10, hid_dim=100, output_dim=9)
+model_2 = component.two_layer_relu(input_dim=10, hid_dim=100, output_dim=9)
 
-model_1 = 
+model_3 = component.three_layer_sigmoid(input_dim=10, hid_dim_1=100, hid_dim_2=100, output_dim=9)
+model_4 = component.three_layer_relu(input_dim=10, hid_dim_1=100, hid_dim_2=100, output_dim=9)
 
 
 
